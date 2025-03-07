7b74609ef2a5cf41e5c1589a26fa4841f872103f
somanathdiksangi <somanathdiksangi@gmail.com>
2025-03-08 02:10:33 +0530

    logging

---
+++ src/mlproject/components/data_ingestion.py
@@ -0,0 +1,10 @@
+import os 
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+
+from dataclasses import dataclass
+@dataclass
+
+
